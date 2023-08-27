024222059c263ea62a462ce71ac649c67efb0e0d
jaytee-padilla <padilla.jaytee@gmail.com>
2023-08-27 10:06:08 -0700

    Finished food_post related routes

--- connection.js
+++ connection.js
@@ -1,12 +1,13 @@
 const mysql = require("mysql");
 require('dotenv').config();
 
+const host = process.env.DB_HOST || "localhost";
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST ? process.env.DB_HOST : "localhost",
+  host: host,
   user: process.env.DB_USER,
   password: process.env.DB_PW,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
 });
 
 connection.connect(function (err) {
--- routes/api/food-routes.js
+++ routes/api/food-routes.js
@@ -8,12 +8,20 @@ router.get("/", (req, res) => {
 	connection.query("SELECT * FROM food_post", function (err, result, fields) {
     if (err) throw err;
 
-    res.send(JSON.stringify(result));
+    return res.send(JSON.stringify(result));
   });
 });
 
 router.get("/:id", (req, res) => {
-	res.send("Hello World!");
+	connection.query("SELECT * FROM food_post WHERE id = ?", req.params.id, (err, result) => {
+		if (err) throw err;
+
+		if (!result || result.length === 0) {
+			return res.send(JSON.stringify({message: "No food post with that id can be found"}));
+		}
+
+		return res.send(JSON.stringify(result));
+	});
 });
 
 router.post("/", (req, res) => {
@@ -30,12 +38,20 @@ router.post("/", (req, res) => {
 	function (err, result, fields) {
     if (err) throw err;
 
-    res.send(JSON.stringify({message: "Successfully added food post to database!"}));
+    return res.send(JSON.stringify({message: "Successfully added food post to database!"}));
   });
 });
 
 router.delete("/:id", (req, res) => {
-	res.send("Hello World!");
+	connection.query("DELETE FROM food_post WHERE id = ?", req.params.id, (err, result) => {
+		if (err) throw err;
+
+		if (!result.affectedRows || result.affectedRows.length === 0) {
+			return res.send(JSON.stringify({message: "No food post with that id can be found"}));
+		}
+
+		return res.send(JSON.stringify({message: "Deleted food post successfully"}));
+	})
 });
 
 module.exports = router;
