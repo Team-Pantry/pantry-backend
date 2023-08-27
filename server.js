42262eaa738800ceaf61521a05fbde374e6fe095
Victoria <Victoria.p.chu@gmail.com>
2023-08-26 18:47:22 -0700

    basic routes

--- server.js
+++ server.js
@@ -15,6 +15,52 @@ app.listen(PORT, (error) => {
 		console.log("Error occurred, server can't start", error);
 });
 
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+  });
+
+app.post("/api/auth/signup", (req, res) => {
+    res.send("Hello World!");
+  });
+app.post("/api/auth/login", (req, res) => {
+    res.send("Hello World!");
+  });
+app.get("/api/auth/user", (req, res) => {
+    res.send("Hello World!");
+  });
+app.get("/api/auth/user/:id", (req, res) => {
+    res.send("Hello World!");
+  });
+app.put("/api/auth/user/:id", (req, res) => {
+    res.send("Hello World!");
+  });
+
+app.get("/api/food", (req, res) => {
+    res.send("Hello World!");
+  });
+app.post("/api/food", (req, res) => {
+    res.send("Hello World!");
+  });
+app.get("/api/food/:id", (req, res) => {
+    res.send("Hello World!");
+  });
+app.delete("/api/food/:id", (req, res) => {
+    res.send("Hello World!");
+  });
+
+app.get("/api/request", (req, res) => {
+    res.send("Hello World!");
+  });
+app.post("/api/request", (req, res) => {
+    res.send("Hello World!");
+  });
+app.get("/api/request/:id", (req, res) => {
+    res.send("Hello World!");
+  });
+app.delete("/api/request/:id", (req, res) => {
+    res.send("Hello World!");
+  });
+
 app.get('/', (req, res) =>
 	res.status(200).json({
 		status: 200,
