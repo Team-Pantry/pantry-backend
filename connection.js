b49ebb4df9238af67ba254a32f7b02e3d1018af6
jaytee-padilla <padilla.jaytee@gmail.com>
2023-08-26 22:15:36 -0700

    Added DB_HOST

--- connection.js
+++ connection.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 
 const connection = mysql.createConnection({
-  host: "localhost",
+  host: process.env.DB_HOST ? process.env.DB_HOST : "localhost",
   user: process.env.DB_USER,
   password: process.env.DB_PW,
   database: process.env.DB_NAME
