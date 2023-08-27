2119ad54f65ba5cb17dbaf929906a1b614bd10af
jaytee-padilla <padilla.jaytee@gmail.com>
2023-08-26 18:13:37 -0700

    Created express server

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+app.listen(PORT, (error) => {
+	if (!error)
+		console.log("Server is Successfully Running, and App is listening on port " + PORT)
+	else
+		console.log("Error occurred, server can't start", error);
+});
+
+app.get('/', (req, res) =>
+	res.status(200).json({
+		status: 200,
+		message: 'The Pantry server is alive and kicking!',
+	}),
+);
